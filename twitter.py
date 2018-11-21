8648cbcbf6df9ef6dc3fe11ad739ec9cb83108a2
babelag <hub.lewandowski@o2.pl>
2018-11-21 19:44:58 +0100

    0.3

--- twitter.py
+++ twitter.py
@@ -1,38 +1,54 @@
+import json
 import re
 import os
+from urllib.parse import urljoin
+import requests
 
+
+
+USERS_API = "https://api.github.com/users"
 
 class Twitter(object):
     version = '1.0'
 
-    def __init__(self, backend=None):
+    def __init__(self, backend=None, username=None):
         self.backend = backend
         self._tweets = []
-        if self.backend and not os.path.exists(self.backend):
-            with open(self.backend,'w'):
-                pass
-
-    def tweet(self, message):
-        if len(message)>160:
-            raise Exception("This message is to long")
-        self.tweets.append(message)
-        if self.backend:
-            with open(self.backend, 'w') as twitter_file:
-                twitter_file.write("\n".join(self.tweets))
+        self.username = username
 
     @property
     def tweets(self):
         if self.backend and not self._tweets:
-            with open(self.backend) as twitter_file:
-                self._tweets = [line.rstrip('\n') for line in twitter_file]
+            backend_text = self.backend.read()
+            if backend_text:
+                self._tweets = json.loads(backend_text)
         return self._tweets
 
-    def delete(self):
-        if self.backend:
-            os.remove(self.backend)
+    @property
+    def tweet_messages(self):
+        return [tweet['message'] for tweet in self.tweets]
 
-    def find_hashtag(self, message):
-        return re.findall("#(\w+)", message)
+    def get_user_avatar(self):
+        if not self.username:
+            return None
+        url = urljoin(USERS_API, self.username)
+        resp = requests.get(url)
+        return resp.json()['avatar_url']
+
+    def tweet(self, message):
+        if len(message)>160:
+            raise Exception("This message is to long")
+        self.tweets.append({'message' : message, 'avatar' : self.get_user_avatar()})
+        if self.backend:
+            self.backend.write(json.dumps(self.tweets))
+
+    #
+    # def delete(self):
+    #     if self.backend:
+    #         os.remove(self.backend)
+    #
+    # def find_hashtag(self, message):
+    #     return re.findall("#(\w+)", message)
 
     def find_hashtag_small_letters(self, message):
-        return [m.lower() for m in re.findall('#(\w+)', message)]
+        return [m.lower() for m in re.findall("#(\w+)", message)]
--- twitter_test.py
+++ twitter_test.py
@@ -1,11 +1,21 @@
 from twitter import Twitter
 import pytest
 
-@pytest.fixture(params=[None, 'test.txt'])
-def twitter(request):
-    twitter = Twitter(backend=request.param)
-    yield twitter
-    twitter.delete()
+#inicjalizacja backend
+@pytest.fixture
+def backend(tmpdir):
+    tmp_file = tmpdir.join('test.txt)')
+    tmp_file.write("")
+    return tmp_file
+
+@pytest.fixture(params=['list', 'backend'], name= 'twitter')
+def fixture_twitter(backend, request):
+    if request.param == 'list':
+        twitter = Twitter()
+    elif request.param == 'backend':
+        twitter = Twitter(backend=backend)
+    return twitter
+
 
 
 def test_twitter_initialization(twitter):
@@ -15,12 +25,21 @@ def test_tweet_single_message(twitter):
     #When
     twitter.tweet("Test message")
     #Than
-    assert twitter.tweets == ["Test message"]
+    assert twitter.tweet_messages == ["Test message"]
 
 def test_tweet_long_message(twitter):
     with pytest.raises(Exception):
         twitter.tweet("test"*41)
-    assert twitter.tweets == []
+    assert twitter.tweet_messages == []
+
+def test_initializing_two_classes(backend):
+    twitter1 = Twitter(backend=backend)
+    twitter2 = Twitter(backend=backend)
+
+    twitter1.tweet('Test 1')
+    twitter1.tweet('Test 2')
+
+    assert twitter2.tweet_messages == ['Test 1', 'Test 2']
 
 # def test_tweet_with_hashtag():
 #     twitter = Twitter()
